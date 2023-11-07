3997e45fdd12f679867f2c58ccfb0bd335ef5a78
mfghir <fatemeghafari77@gmail.com>
2023-11-07 01:31:12 -0800

    add url

---
+++ js/game.js
@@ -0,0 +1,20 @@
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+let formattedData = null;
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
