847641e648ae70a456e4a1c8d4775bb6f054053f
mfghir <fatemeghafari77@gmail.com>
2023-11-08 00:37:59 -0800

    showing next question

--- js/game.js
+++ js/game.js
@@ -6,8 +6,12 @@ const URL =
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
+
 const answerList = document.querySelectorAll(".answer-text");
 const scoreText = document.getElementById("score");
+const nextButton = document.getElementById("next-button");
+
+const questionNumber = document.getElementById("question-number");
 
 const CORRECT_BONUS = 10;
 
@@ -32,8 +36,10 @@ const start = () => {
 };
 
 const showQuestion = () => {
+  questionNumber.innerText = questionIndex + 1;
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
+
   correctAnswer = correctAnswerIndex;
   questionText.innerText = question;
 
@@ -45,7 +51,7 @@ const showQuestion = () => {
 const checkAnswer = (e, index) => {
   if (!isAccepted) return;
   isAccepted = false;
-  
+
   const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     e.target.classList.add("correct");
@@ -57,7 +63,23 @@ const checkAnswer = (e, index) => {
   }
 };
 
+const nextHandler = () => {
+  questionIndex++;
+  if (questionIndex < formattedData.length) {
+    isAccepted = true;
+    removeClasses();
+    showQuestion();
+  } else {
+    window.location.assign("/end.html");
+  }
+};
+
+const removeClasses = () => {
+  answerList.forEach((button) => (button.className = "answer-text"));
+};
+
 window.addEventListener("load", fetchData);
+nextButton.addEventListener("click", nextHandler);
 answerList.forEach((button, index) => {
   button.addEventListener("click", (e) => checkAnswer(e, index));
 });
